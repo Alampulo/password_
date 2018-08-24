a8a368d5c54c21ac06bce97d921640b909a35454
Bryan Juma <jumabryan10@gmail.com>
2018-08-24 16:27:35 +0300

    1st init test added

--- password_test.py
+++ password_test.py
@@ -14,3 +14,15 @@ class TestPassword(unittest.TestCase):
      Set up method to run before each test cases.
      '''
       self.new_user = User("first_name","last_name","special_key")
+
+   def test_init(self):
+        '''
+        test_init test case to test if the object is initialized properly
+        '''
+   self.assertEqual(self.new_user.first_name,"buju")
+   self.assertEqual(self.new_user.last_name,"bantom")
+   self.assertEqual(self.new_user.special_key,"13039042")
+
+
+  if __name__ == '__main__':
+    unittest.main()
