41a73d7571d00d8c4dd15a2031f9722b6385b269
Bryan Juma <jumabryan10@gmail.com>
2018-08-24 16:43:36 +0300

    2nd test added

--- password_test.py
+++ password_test.py
@@ -9,6 +9,14 @@ class TestPassword(unittest.TestCase):
        self.assertEqual(self.new_user.first_name,"buju")
        self.assertEqual(self.new_user.last_name,"bantom")
        self.assertEqual(self.new_user.special_key,"13039042")
-
-if __name__ == '__main__':
+  # test2
+  if __name__ == '__main__':
     unittest.main()
+
+        def test_save_password(self):
+
+        self.new_password.save_password() # saving the new contact
+        self.assertEqual(len(Password.password_list),1)
+
+ if __name__ ==  '__main__':
+    unittest.main(
