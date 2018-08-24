c459881182649f3f827cc504adaa5a9c73a8fd18
Bryan Juma <jumabryan10@gmail.com>
2018-08-24 16:38:56 +0300

    deletes comments

--- password.py
+++ password.py
@@ -4,11 +4,10 @@
   """
 
   def  __init__(self,first_name,last_name,special_key):
-
         self.first_name = first_name
         self.last_name = last_name
         self.special_key = special_key
 
 class Password:
-    
+
        password_list = []
--- password_test.py
+++ password_test.py
@@ -1,28 +1,14 @@
 import unittest
-from Password import Password
+from password import Password
 
 class TestPassword(unittest.TestCase):
-     '''
-    Test class that defines test cases for the contact class behaviours.
+    def setUp(self):
+     self.new_user = User("first_name","last_name","special_key")
 
-    Args:
-        unittest.TestCase: TestCase class that helps in creating test cases
-    '''
+    def test_init(self):
+       self.assertEqual(self.new_user.first_name,"buju")
+       self.assertEqual(self.new_user.last_name,"bantom")
+       self.assertEqual(self.new_user.special_key,"13039042")
 
-  def setUp(self):
-     '''
-     Set up method to run before each test cases.
-     '''
-      self.new_user = User("first_name","last_name","special_key")
-
-   def test_init(self):
-        '''
-        test_init test case to test if the object is initialized properly
-        '''
-   self.assertEqual(self.new_user.first_name,"buju")
-   self.assertEqual(self.new_user.last_name,"bantom")
-   self.assertEqual(self.new_user.special_key,"13039042")
-
-
-  if __name__ == '__main__':
+if __name__ == '__main__':
     unittest.main()
