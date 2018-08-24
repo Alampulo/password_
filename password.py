d2184945d88b93c0406ab8cedc79b80e5eae7c1c
Bryan Juma <jumabryan10@gmail.com>
2018-08-24 15:58:33 +0300

    user class added

---
+++ password.py
@@ -0,0 +1,10 @@
+ class User :
+  """
+  Class that generates new instances of Passwords
+  """
+
+  def  __init__(self,first_name,last_name,special_key):
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.special_key = special_key
