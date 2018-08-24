ed28eafbea1b37a3a92d9d538f641d5c74e92644
Bryan Juma <jumabryan10@gmail.com>
2018-08-24 16:11:40 +0300

    password_test class added

---
+++ password_test.py
@@ -0,0 +1,10 @@
+import unittest
+from Password import Password
+
+class TestPassword(unittest.TestCase):
+     '''
+    Test class that defines test cases for the contact class behaviours.
+
+    Args:
+        unittest.TestCase: TestCase class that helps in creating test cases
+    '''
