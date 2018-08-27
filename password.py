e06b10406a82250d24bdff5905381ece829ee152
Bryan Juma <jumabryan10@gmail.com>
2018-08-27 08:04:22 +0300

    generate random passwords added

--- password.py
+++ password.py
@@ -1,18 +1,27 @@
 import random
 import pyperclip
- class User :
-  """
-  Class that generates new instances of Passwords
-  """
+class user:
+    '''
+    class that generates new instances of user account
+    '''
+    user_list = []
+    def __init__(self,name,pwd):
+        self.name = name
+        self.pwd = pwd
 
-  def  __init__(self,first_name,last_name,special_key):
-        self.first_name = first_name
-        self.last_name = last_name
-        self.special_key = special_key
+    def save_user(self):
+        '''
+        save user details into user_list
+        '''
+        user.user_list.append(self)
 
-class Password:
-
-       password_list = []
- def save_password(self):
-
-        Password.password_list.append(self)
+    def generate_password(self):
+        '''
+        generate new password
+        '''
+        chars = '1234567890abcdefghijklmnop?/@-' #characters to choose from
+        length = int(input("Enter the length of password you want: "))
+        pwd = ''
+        for c in range(length):
+            pwd += random.choice(chars) #generate random password
+        print (pwd)
