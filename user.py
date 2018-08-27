d40f70b9c48b0a7c3ccfd62b0dd403e908a249dc
Bryan Juma <jumabryan10@gmail.com>
2018-08-27 07:56:00 +0300

    user class import addedy

---
+++ user.py
@@ -0,0 +1,11 @@
+import random
+
+class user:
+    """
+    creates a class user that acts as a blueprint for all object instances of a user
+    """
+
+    user_list = []
+    """
+    created a list where all new user objects will be stored
+    """
